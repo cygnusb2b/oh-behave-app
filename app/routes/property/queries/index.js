d2b990bd3a7f0cab3d72e36ca4bff6dc98d20ab6
Jacob Bare <jacob@limit0.io>
2018-04-18 19:30:47 -0500

    Add query list view

--- app/routes/property/queries/index.js
+++ app/routes/property/queries/index.js
@@ -1,8 +1,44 @@
 import Route from '@ember/routing/route';
+import RouteQueryManager from 'ember-apollo-client/mixins/route-query-manager';
 
-export default Route.extend({
-  model() {
-    const property = this.modelFor('property');
-    return { property };
+import query from 'oh-behave-app/gql/queries/all-content-queries';
+
+export default Route.extend(RouteQueryManager, {
+  queryParams: {
+    first: {
+      refreshModel: true
+    },
+    after: {
+      refreshModel: true
+    },
+    sortBy: {
+      refreshModel: true
+    },
+    ascending: {
+      refreshModel: true
+    },
+  },
+
+  setPagination(pagination) {
+    const { totalCount } = pagination;
+    const { hasNextPage, endCursor } = pagination.pageInfo;
+    this.controllerFor('property.queries.index').setProperties({ totalCount, hasNextPage, endCursor });
+    return pagination.edges.map(node => node.node);
+  },
+
+  model({ first, after, sortBy, ascending }) {
+    const pagination = { first, after };
+    const sort = { field: sortBy, order: ascending ? 1 : -1 };
+    const variables = { pagination, sort };
+    if (!sortBy) delete variables.sort.field;
+    return this.get('apollo').watchQuery({ query, variables, fetchPolicy: 'network-only' }, 'allContentQueries')
+      .then(pagination => this.setPagination(pagination))
+      .catch(e => this.get('graphErrors').show(e))
+    ;
+  },
+
+  setupController(controller, model) {
+    this._super(controller, model);
+    controller.set('property', this.modelFor('property'));
   },
 });

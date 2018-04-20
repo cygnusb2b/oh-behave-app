30eeb3af8d2ce6eff7e4aaa1d7dcd1ae6faf6af3
Jacob Bare <jacob@limit0.io>
2018-04-20 11:29:20 -0500

    Create menu mixin and apply to sort

--- app/components/list-controls/sort.js
+++ app/components/list-controls/sort.js
@@ -1,8 +1,9 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
 import { isArray } from '@ember/array';
+import MenuMixin from 'oh-behave-app/components/list-controls/menu-mixin';
 
-export default Component.extend({
+export default Component.extend(MenuMixin, {
   classNames: ['btn-group'],
   attributeBindings: ['role', 'aria-label'],
 
@@ -38,22 +39,6 @@ export default Component.extend({
   buttonClass: 'btn-primary',
 
   /**
-   * The dropdown menu direction.
-   * An empty value will use the default `down` direction.
-   * Can be one of `up`, `right` or `left`.
-   * @public
-   * @type {string}
-   */
-  direction: null,
-
-  /**
-   * Aligns the menu.
-   * An empty value will use the default `left` alignment.
-   * A value of `right` will right-align the menu.
-   */
-  alignment: null,
-
-  /**
    * Based on the `sortBy` value, computes the selected sort object.
    * For example, if the `sortBy` value equals `createdAt`, this would return
    * something like `{ key: 'createdAt', label: 'Created' }`.
@@ -68,30 +53,6 @@ export default Component.extend({
    */
   filteredOptions: computed('options.[]', 'sortBy', function() {
     return this.get('options').rejectBy('key', this.get('sortBy'));
-  }),
-
-  /**
-   * Determines the menu direction class based off the `direction` property.
-   */
-  directionClass: computed('direction', function() {
-    switch (this.get('direction')) {
-      case 'up':
-        return 'dropup';
-      case 'left':
-        return 'dropleft';
-      case 'right':
-        return 'dropright';
-      default:
-        return '';
-    }
-  }),
-
-  /**
-   * Determines the menu alignment class based off the `alignment` property.
-   */
-  alignmentClass: computed('alignment', function() {
-    if (this.get('alignment') === 'right') return 'dropdown-menu-right';
-    return '';
   }),
 
   /**

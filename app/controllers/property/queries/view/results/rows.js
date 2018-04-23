6494e196ad7fae3a5c527a879bc798b0fc11e956
Jacob Bare <jacob@limit0.io>
2018-04-23 15:43:42 -0500

    Handle exports

--- app/controllers/property/queries/view/results/rows.js
+++ app/controllers/property/queries/view/results/rows.js
@@ -1,7 +1,10 @@
 import ListController from '../../../../abstract-list';
+import { inject } from '@ember/service';
 import LoadingMixin from 'oh-behave-app/mixins/loading-mixin';
 
 export default ListController.extend(LoadingMixin, {
+  session: inject(),
+
   init() {
     this._super(...arguments);
     this.set('sortOptions', [
@@ -10,5 +13,11 @@ export default ListController.extend(LoadingMixin, {
     ]);
     this.set('sortBy', 'email');
     this.set('ascending', true);
+  },
+
+  actions: {
+    export(resultId) {
+      window.location = `/export/${resultId}?bearer=${this.get('session.data.authenticated.token')}`;
+    },
   },
 });

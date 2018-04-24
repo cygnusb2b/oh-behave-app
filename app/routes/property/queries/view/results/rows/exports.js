78479e17d000a4cf12fa23cee1009cddcf647260
Jacob Bare <jacob@limit0.io>
2018-04-24 13:16:04 -0500

    Add exports query for modal

--- app/routes/property/queries/view/results/rows/exports.js
+++ app/routes/property/queries/view/results/rows/exports.js
@@ -1,4 +1,19 @@
 import Route from '@ember/routing/route';
+import RouteQueryManager from 'ember-apollo-client/mixins/route-query-manager';
 
-export default Route.extend({
+import query from 'oh-behave-app/gql/queries/all-exports-for-query-result';
+
+export default Route.extend(RouteQueryManager, {
+
+  model() {
+    const resultId = this.modelFor('property.queries.view.results.rows').result.get('id');
+
+    const pagination = { first: 100 };
+    const sort = { field: 'exportedAt', order:  -1 };
+    const variables = { resultId, pagination, sort };
+    return this.get('apollo').watchQuery({ query, variables, fetchPolicy: 'network-only' }, 'allExportsForQueryResult')
+      .then(pagination => pagination.edges.map(node => node.node))
+      .catch(e => this.get('graphErrors').show(e))
+    ;
+  },
 });

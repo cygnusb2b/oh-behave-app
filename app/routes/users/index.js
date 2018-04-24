46c3d4c8d4f4852d2bf027c8d06ecd0c72773e0a
Jacob Bare <jacob@limit0.io>
2018-04-24 14:44:09 -0500

    Add load more

--- app/routes/users/index.js
+++ app/routes/users/index.js
@@ -1,5 +1,6 @@
 import Route from '@ember/routing/route';
 import RouteQueryManager from 'ember-apollo-client/mixins/route-query-manager';
+import { getObservable } from 'ember-apollo-client';
 
 import query from 'oh-behave-app/gql/queries/all-users';
 
@@ -19,21 +20,18 @@ export default Route.extend(RouteQueryManager, {
     },
   },
 
-  setPagination(pagination) {
-    const { totalCount } = pagination;
-    const { hasNextPage, endCursor } = pagination.pageInfo;
-    this.controllerFor('users.index').setProperties({ totalCount, hasNextPage, endCursor });
-    return pagination.edges.map(node => node.node);
-  },
-
   model({ first, after, sortBy, ascending }) {
+    const controller = this.controllerFor(this.get('routeName'));
+
     const pagination = { first, after };
     const sort = { field: sortBy, order: ascending ? 1 : -1 };
     const variables = { pagination, sort };
     if (!sortBy) delete variables.sort.field;
     return this.get('apollo').watchQuery({ query, variables, fetchPolicy: 'network-only' }, 'allUsers')
-      .then(pagination => this.setPagination(pagination))
-      .catch(e => this.get('graphErrors').show(e))
+      .then((result) => {
+        controller.set('observable', getObservable(result));
+        return result;
+      }).catch(e => this.get('graphErrors').show(e))
     ;
   },
 

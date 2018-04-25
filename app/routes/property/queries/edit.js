f4536930a0ca4bd93eb142af472ad2a223ad7a4d
Jacob Bare <jacob@limit0.io>
2018-04-25 11:36:04 -0500

    Add query edit support

--- app/routes/property/queries/edit.js
+++ app/routes/property/queries/edit.js
@@ -3,6 +3,7 @@ import RouteQueryManager from 'ember-apollo-client/mixins/route-query-manager';
 import LoadingMixin from 'oh-behave-app/mixins/loading-mixin';
 
 import query from 'oh-behave-app/gql/queries/content-query';
+import updateContentQuery from 'oh-behave-app/gql/mutations/update-content-query';
 
 export default Route.extend(RouteQueryManager, LoadingMixin, {
   model({ query_id }) {
@@ -14,5 +15,31 @@ export default Route.extend(RouteQueryManager, LoadingMixin, {
   setupController(controller, model) {
     this._super(controller, model);
     controller.set('property', this.modelFor('property'));
+  },
+
+  actions: {
+    update(model) {
+      this.showLoading();
+      const propertyId = this.modelFor('property').get('id');
+      const mutation = updateContentQuery;
+      const { id, name, criteria } = model;
+
+      const formatted = criteria.reduce((agg, group) => {
+        const { type, items } = group;
+        const ids = items.map(item => item.id);
+        agg.push({ type, ids });
+        return agg;
+      }, []);
+
+      const payload = { name, criteria: formatted, propertyId };
+      const input = { id, payload };
+      const variables = { input };
+      return this.get('apollo').mutate({ mutation, variables }, 'updateContentQuery')
+        .then(() => this.get('notify').info('Query successfully updated.'))
+        .catch(e => this.get('graphErrors').show(e))
+        .finally(() => this.hideLoading())
+      ;
+
+    },
   },
 });

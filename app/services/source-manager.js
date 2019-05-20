da84f965c9927329c55576814dd658d6c1237dce
Jacob Bare <jacob@limit0.io>
2019-05-20 11:24:14 -0500

    Handle base stacks on property edit/list

--- app/services/source-manager.js
+++ app/services/source-manager.js
@@ -5,5 +5,6 @@ export default Service.extend({
     this._super(...arguments);
     this.set('baseVersions', ['3', '4']);
     this.set('userSources', ['Merrick', 'Components', 'Radix']);
+    this.set('stacks', ['aerilon', 'picon', 'caprica']);
   },
 });
